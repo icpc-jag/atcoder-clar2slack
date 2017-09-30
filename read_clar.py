703e25e6b74f0b52ebc99a727515d1f0acb7ac76
hasi <hasipon@users.noreply.github.com>
2017-09-30 11:58:55 +0000

    <tbody> not found

--- read_clar.py
+++ read_clar.py
@@ -13,27 +13,30 @@ def func(s):
 data = []
 with open(sys.argv[1], encoding="utf-8") as f:
     a = re.findall(r"<tbody>.*?</tbody>", f.read(), re.S)
-    assert len(a) == 1
-    a = re.findall(r"<tr>.*?</tr>", a[0], re.S)
-    for x in a:
-        b = re.findall(r"<td.*?</td>", x, re.S)
-        title = func(b[0])
-        user_name = func(b[1])
-        assert re.fullmatch(r"<td>.*</td>", b[2], re.S)
-        assert re.fullmatch(r"<td>.*</td>", b[3], re.S)
-        question = re.sub(r"<.*?>", "", b[2][4:-5]).replace("&#039;", "'")
-        response = re.sub(r"<.*?>", "", b[3][4:-5]).replace("&#039;", "'")
-        public = re.sub(r"<.*?>", "", b[4])
-        update_url = CONTEST_URL + re.search(r"<a href=\"(.*?)\">", b[7]).group(1)
+    if not a:
+        print("<tbody> not found")
+    else:
+        assert len(a) == 1
+        a = re.findall(r"<tr>.*?</tr>", a[0], re.S)
+        for x in a:
+            b = re.findall(r"<td.*?</td>", x, re.S)
+            title = func(b[0])
+            user_name = func(b[1])
+            assert re.fullmatch(r"<td>.*</td>", b[2], re.S)
+            assert re.fullmatch(r"<td>.*</td>", b[3], re.S)
+            question = re.sub(r"<.*?>", "", b[2][4:-5]).replace("&#039;", "'")
+            response = re.sub(r"<.*?>", "", b[3][4:-5]).replace("&#039;", "'")
+            public = re.sub(r"<.*?>", "", b[4])
+            update_url = CONTEST_URL + re.search(r"<a href=\"(.*?)\">", b[7]).group(1)
 
-        data.append({
-            "title": title,
-            "user_name": user_name,
-            "question": question,
-            "response": response,
-            "public": public,
-            "update_url": update_url,
-        })
+            data.append({
+                "title": title,
+                "user_name": user_name,
+                "question": question,
+                "response": response,
+                "public": public,
+                "update_url": update_url,
+            })
 
 with open(SAVE_PATH, "w", encoding="utf8") as f:
     f.write(json.dumps(data, ensure_ascii=False))
